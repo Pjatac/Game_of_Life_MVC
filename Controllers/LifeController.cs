c9f54a1fc34f5865b652875f7ef6b154904982c6
Pjatak <pjataka@gmail.com>
2020-10-04 16:34:48 +0300

    Init

---
+++ Controllers/LifeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Game_of_Life_MVC.Controllers
+{
+	public class LifeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Game_of_Life_MVC.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
